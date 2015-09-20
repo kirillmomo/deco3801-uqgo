5060444dff62a85b7a81ba6e80adec027115182e
Sherwin Saran <sherwin.saran@gmail.com>
2015-09-20 13:15:57 +1000

    Hardcoded friends list

--- web-app-new/php/function/get_friends_list.php
+++ web-app-new/php/function/get_friends_list.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 	$rank_by = $_GET['rankBy']; // getting ranking option from the ajax request
 	for($i = 0; $i<sizeof($friend_list_display); $i++)
 	{
@@ -6,10 +6,10 @@
 	<li onClick="showProfile('echo user_id here', this);"><div class="friend-image"></div><p><?php echo $friend_list_display[$i]; ?></p></li>
 	<?php
 	}
-?>
+?> -->
 
 <!-- EXAMPLE FRIENDS LIST - friends should be echoed like below -->
-<!-- <li onClick="showProfile('1', this);"><div class="friend-image"></div><p>Johnson Carter</p></li>
+<li onClick="showProfile('1', this);"><div class="friend-image"></div><p>Johnson Carter</p></li>
 <li onClick="showProfile('2', this);"><div class="friend-image"></div><p>Johnson Jackson</p></li>
 <li onClick="showProfile('3', this);"><div class="friend-image"></div><p>Jenson Carter</p></li>
-<li onClick="showProfile('4', this);"><div class="friend-image"></div><p>Jenson Jackson</p></li> -->
+<li onClick="showProfile('4', this);"><div class="friend-image"></div><p>Jenson Jackson</p></li>
