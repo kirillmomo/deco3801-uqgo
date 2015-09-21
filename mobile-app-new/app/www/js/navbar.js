677030a23e190a96b755d99ba11002b539307177
ChamathAbey <chamath.abey@gmail.com>
2015-09-21 23:31:40 +1000

    Updated User Interface
    Added tabs to all sections and added Icons to the bottom navigation bar.
    Social tabs have been structured.

---
+++ mobile-app-new/app/www/js/navbar.js
@@ -0,0 +1,19 @@
+function dashboard() {
+    window.location = "dashboard.html";   
+}
+
+function social() {
+    window.location = "social.html";   
+}
+
+function index() {
+    window.location = "index.html";   
+}
+
+function challenge() {
+    window.location = "challenge.html";   
+}
+
+function settings() {
+    window.location = "settings.html";   
+}
